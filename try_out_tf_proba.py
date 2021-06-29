0fed5e100c8d5615f4ff561cd4e415f1298b88ac
Marcus Tuke <marcustuke@tukenukem.home>
2021-06-29 21:38:26 +0100

    small edit to sts function

--- try_out_tf_proba.py
+++ try_out_tf_proba.py
@@ -16,22 +16,22 @@ if tf.test.gpu_device_name() != "/device:GPU:0":
 else:
     print("SUCCESS: Found GPU: {}".format(tf.test.gpu_device_name()))
 
-def tfp_sts_forecast(series, n_seasons, n_steps_forecast, param_samples, forecast_samples):
-    trend = tfp.sts.LocalLinearTrend(observed_time_series=co2_by_month)
+def tfp_sts_forecast(series, n_seasons=12, n_steps_forecast=20, param_samples=50, forecast_samples=10):
+    trend = tfp.sts.LocalLinearTrend(observed_time_series=series)
     seasonal = tfp.sts.Seasonal(
-        num_seasons=12, observed_time_series=co2_by_month)
+        num_seasons=n_seasons, observed_time_series=series)
 
-    model = tfp.sts.Sum([trend, seasonal], observed_time_series=co2_by_month)
+    model = tfp.sts.Sum([trend, seasonal], observed_time_series=series)
     variational_posteriors = tfp.sts.build_factored_surrogate_posterior(model=model)
 
     forecast = tfp.sts.forecast(
-        model, observed_time_series=co2_by_month,
-        num_steps_forecast=50,
-        parameter_samples=variational_posteriors.sample(50)
+        model, observed_time_series=series,
+        num_steps_forecast=n_steps_forecast,
+        parameter_samples=variational_posteriors.sample(param_samples)
     )
 
     return (
             forecast.mean()[..., 0],
             forecast.stddev()[..., 0],
-            forecast.sample(10)[..., 0],
+            forecast.sample(forecast_samples)[..., 0],
     )
